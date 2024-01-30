a990b3cfce8a942a6da696b5aaf9c551a3ee734e
Florian Lanzinger <lanzinger@kit.edu>
2024-01-30 18:24:00 +0100

    Track fields of superclasses in initial excl store

--- src/main/java/edu/kit/kastel/property/subchecker/exclusivity/ExclusivityTransfer.java
+++ src/main/java/edu/kit/kastel/property/subchecker/exclusivity/ExclusivityTransfer.java
@@ -1,7 +1,11 @@
 package edu.kit.kastel.property.subchecker.exclusivity;
 
+import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodTree;
 
+import com.sun.tools.javac.tree.JCTree;
+import edu.kit.kastel.property.packing.PackingFieldAccessAnnotatedTypeFactory;
+import edu.kit.kastel.property.packing.PackingFieldAccessSubchecker;
 import edu.kit.kastel.property.subchecker.exclusivity.rules.*;
 
 import org.checkerframework.dataflow.analysis.RegularTransferResult;
@@ -9,12 +13,22 @@ import org.checkerframework.dataflow.analysis.TransferInput;
 import org.checkerframework.dataflow.analysis.TransferResult;
 import org.checkerframework.dataflow.cfg.UnderlyingAST;
 import org.checkerframework.dataflow.cfg.node.*;
+import org.checkerframework.dataflow.expression.FieldAccess;
+import org.checkerframework.dataflow.expression.ThisReference;
 import org.checkerframework.framework.flow.CFAbstractAnalysis;
 import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFTransfer;
 import org.checkerframework.framework.flow.CFValue;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.util.AnnotatedTypes;
+import org.checkerframework.javacutil.ElementUtils;
+import org.checkerframework.javacutil.TreeUtils;
+import org.checkerframework.javacutil.TypesUtils;
 
 import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
 import java.util.List;
 
 public class ExclusivityTransfer extends CFTransfer {
@@ -70,5 +84,66 @@ public class ExclusivityTransfer extends CFTransfer {
         new TAssign(store, factory, analysis).applyOrInvalidate(returnTypeAnno, node.getResult());
 
         return new RegularTransferResult<>(null, store);
+    }
+
+    @Override
+    public CFStore initialStore(UnderlyingAST underlyingAST, List<LocalVariableNode> parameters) {
+        CFStore initStore = super.initialStore(underlyingAST, parameters);
+
+        // The default implementation only adds fields declared in this class.
+        // To make type-checking of pack statements more precise, we also add all fields declared in superclasses.
+        if (underlyingAST.getKind() == UnderlyingAST.Kind.METHOD) {
+            UnderlyingAST.CFGMethod method = (UnderlyingAST.CFGMethod) underlyingAST;
+            MethodTree methodDeclTree = method.getMethod();
+            ExecutableElement methodElem = TreeUtils.elementFromDeclaration(methodDeclTree);
+            ClassTree classTree = method.getClassTree();
+
+            if (!ElementUtils.isStatic(TreeUtils.elementFromDeclaration(methodDeclTree))) {
+                List<VariableElement> allFields = ElementUtils.getAllFieldsIn(
+                        TypesUtils.getTypeElement(((JCTree) classTree).type),
+                        factory.getElementUtils());
+                AnnotatedTypeMirror receiverType =
+                        factory.getSelfType(methodDeclTree.getBody());
+
+                PackingFieldAccessAnnotatedTypeFactory packingFactory =
+                        factory.getChecker().getTypeFactoryOfSubcheckerOrNull(PackingFieldAccessSubchecker.class);
+
+                AnnotatedTypeMirror receiverPackingType =
+                        packingFactory.getSelfType(methodDeclTree.getBody());
+
+                for (VariableElement field : allFields) {
+                    if (!ElementUtils.isStatic(field)) {
+                        FieldAccess fieldAccess = new FieldAccess(new ThisReference(methodElem.getReceiverType()), field);
+                        TypeMirror fieldOwnerType = field.getEnclosingElement().asType();
+                        AnnotatedTypeMirror declaredType = factory.getAnnotatedType(field);
+
+                        // Viewpoint-adapt type
+                        AnnotatedTypeMirror adaptedType =
+                                AnnotatedTypes.asMemberOf(
+                                        analysis.getTypes(),
+                                        analysis.getTypeFactory(),
+                                        receiverType,
+                                        field,
+                                        declaredType);
+
+                        if (adaptedType == null) {
+                            adaptedType = declaredType;
+                        }
+
+                        // Use @ReadOnly if receiver is not sufficiently packed
+                        if (!packingFactory.isInitializedForFrame(receiverPackingType, fieldOwnerType)) {
+                            adaptedType.clearAnnotations();
+                            adaptedType.addAnnotations(factory.getQualifierHierarchy().getTopAnnotations());
+                        }
+
+                        CFValue value = analysis.createAbstractValue(adaptedType);
+
+                        initStore.insertValue(fieldAccess, value);
+                    }
+                }
+            }
+        }
+
+        return initStore;
     }
 }
--- src/main/java/edu/kit/kastel/property/util/Packing.java
+++ src/main/java/edu/kit/kastel/property/util/Packing.java
@@ -1,10 +1,14 @@
 package edu.kit.kastel.property.util;
 
+import org.checkerframework.dataflow.qual.Pure;
+
 public final class Packing {
 
     private Packing() { }
 
+    @Pure
     public static <T> void pack(T t, Class<? super T> as) {}
 
+    @Pure
     public static <T> void unpack(T t, Class<? super T> from) {}
 }
