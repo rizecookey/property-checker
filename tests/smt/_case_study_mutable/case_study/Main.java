731f3186471f27a28b93bcc4c7ed195cee1817e8
JohnnyJayJay <johnny@leftfold.tech>
2025-02-27 20:41:30 +0100

    simplify smt case study main class further

--- tests/smt/_case_study_mutable/case_study/Main.java
+++ tests/smt/_case_study_mutable/case_study/Main.java
@@ -20,37 +20,19 @@ public final class Main {
         Product product18 = new Product("Louisiana Buzzsaw Carnage", 10, 18);
         Customer customer18 = new Customer("Alice", 18);
         Shop shop = new Shop();
-
         // :: error: agedover.argument.type.incompatible
-        addOrder(shop, customer18, product18);
-
+        shop.addOrder(new Order(customer18, product18));
         Product product6 = new Product("Tim & Jeffrey, All Episodes", 10, 6);
-
         // :: error: agedover.argument.type.incompatible
-        addOrder(shop, customer18, product6);
-
+        shop.addOrder(new Order(customer18, product6));
         Customer customer14 = new Customer("Bob", 14);
 
         // :: error: agedover.argument.type.incompatible
-        addOrder(shop, customer14, product6);
+        shop.addOrder(new Order(customer14, product6));
 
         shop.processNextOrder();
         shop.processNextOrder();
         shop.processNextOrder();
-
         shop.processNextOrder();
-    }
-
-    @JMLClauseTranslationOnly("requires \\invariant_for(shop);")
-    @JMLClauseTranslationOnly("ensures \\invariant_for(shop);")
-    @JMLClauseTranslationOnly("assignable shop.orders.footprint;")
-    // :: error: agedover.contracts.postcondition.not.satisfied
-    public static void addOrder(@Unique Shop shop,
-                                @MaybeAliased @AgedOver(age="product.ageRestriction") Customer customer,
-                                @MaybeAliased Product product) {
-        // :: error: agedover.argument.type.incompatible
-        shop.addOrder(new Order(customer, product));
-        Assert.immutableFieldUnchanged("customer", "customer.age");
-        Assert.immutableFieldUnchanged("product", "product.ageRestriction");
     }
 }
